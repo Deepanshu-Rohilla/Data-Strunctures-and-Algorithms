0b8ae94b1cb036eda722756005d9886d4ea5c90b
Deepanshu Rohilla <deepanshu38841@gmail.com>
2020-07-20 16:43:55 +0530

    postoder traversal in trees

---
+++ trees/postorderTraversal.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left, *right;
+    Node(int data){
+        this->data = data;
+        left = right = NULL;
+    }
+};
+void postorderTraversal(Node *node){
+    if(node==NULL){
+        return;
+    }
+    postorderTraversal(node->left);
+    postorderTraversal(node->right);
+    cout<<node->data<<" ";
+}
+
+int main(){
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Postorder traversal of binary tree is \n";
+    postorderTraversal(root);
+    return 0;
+}
