efcae9dd21a112afe4246e07f9266da8d3ce8758
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-05-31 09:52:42 +0530

    arrays1

---
+++ gfg problems/arrrays/1.cpp
@@ -0,0 +1,30 @@
+class Solution{
+    public:
+    void sort012(int a[], int n)
+    {
+        int b1=0;
+        int b2=0;
+        int b3=0;
+        for(int i=0;i<n;i++){
+            if (a[i]==0){
+                b1++;
+            }
+            else if(a[i]==1){
+                b2++;
+            }
+            else{
+                b3++;
+            }
+        }
+        for(int i=0;i<b1;i++){
+            a[i]=0;
+        }
+        for(int i=b1;i<b1+b2;i++){
+            a[i]=1;
+        }
+        for(int i=b1+b2;i<n;i++){
+            a[i]=2;
+        }
+        
+    }
+};
