2341f10e935bce9694a4e4cfd09ce9c6674415cd
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-06-07 18:19:39 +0530

    linkedlists3

---
+++ gfg problems/linkedList/3.cpp
@@ -0,0 +1,29 @@
+Rotate a Linked List
+https://practice.geeksforgeeks.org/problems/rotate-a-linked-list/1#
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        if(head->next==NULL){
+            return head;
+        }
+        Node* temp = head;
+        while(k!=1){
+            head = head->next;
+            k--;
+        }
+        if(head->next==NULL){
+            return temp;
+        }
+        Node* ans = head->next;
+        head->next = NULL;
+        Node* temp2 = ans;
+        while(temp2->next!=NULL){
+            temp2 = temp2->next;
+        }
+        temp2->next = temp;
+        return ans;
+    }
+};
