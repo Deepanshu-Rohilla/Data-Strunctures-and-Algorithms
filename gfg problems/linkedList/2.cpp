0a9444e679e67f3a50aea5f1e4069c0d4f01cc56
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-06-07 18:08:13 +0530

    linkedlists2

---
+++ gfg problems/linkedList/2.cpp
@@ -0,0 +1,23 @@
+Reverse a linked list
+https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        if(head->next==NULL){
+            return head;
+        }
+        Node* prev = NULL;
+        Node* next = NULL;
+        while(head!=NULL){
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    
+};
