cebfbdcb02ecac01d057d25ced6a6fea2f586130
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-06-21 13:57:52 +0530

    trees5

---
+++ gfg problems/tree/5.cpp
@@ -0,0 +1,14 @@
+Count Leaves in Binary Tree
+https://practice.geeksforgeeks.org/problems/count-leaves-in-binary-tree/1
+int countLeaves(Node* root)
+{
+    if(root==NULL){
+        return 0;
+    }
+  if(root->left==NULL && root->right==NULL){
+      return 1;
+  }
+  return countLeaves(root->left) + countLeaves(root->right);
+  
+}
+
