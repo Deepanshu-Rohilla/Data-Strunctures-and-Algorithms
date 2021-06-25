9dcf9632de3148af899bff755c5bb173345b8346
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-06-25 22:36:41 +0530

    trees6

---
+++ gfg problems/tree/6.cpp
@@ -0,0 +1,16 @@
+Mirror Tree 
+https://practice.geeksforgeeks.org/problems/mirror-tree/1
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        if(node==NULL){
+            return;
+        }
+        Node* temp = node->left;
+        node->left = node->right;
+        node->right = temp;
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
