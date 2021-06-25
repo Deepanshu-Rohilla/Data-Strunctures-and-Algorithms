c7b22ed53c6cd7d532cba8b325fcdd2ba08822ce
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-06-25 19:23:52 +0530

    bits2

--- gfg problems/bit/2.cpp
+++ gfg problems/bit/2.cpp
@@ -6,7 +6,7 @@ class Solution
     //Function to find the first position with different bits.
     int posOfRightMostDiffBit(int m, int n)
     {
-        int n1 = m^n;
-        return log2(n1&-n1) + 1;
+        int n1 = m^n; // To get the number which has 1 only at diff locations
+        return log2(n1&-n1) + 1; // Get rightmost 1 i.e rightmost diff
     }
 };
