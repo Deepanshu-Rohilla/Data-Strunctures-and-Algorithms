0931fd47141fe7abbb9337d4c67c4f2bdd869408
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-06-25 21:23:59 +0530

    bits5

---
+++ gfg problems/bit/5.cpp
@@ -0,0 +1,13 @@
+Power of 2 
+https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1#
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        if(n==0){
+            return false;
+        }
+        // return ceil(log2(n))==floor(log2(n));
+        return !(n&(n-1));
+    }
+};
