6bfab90c24b8d731adb234c736c86023c8b3b357
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-07-12 01:48:43 +0530

    hashing1

---
+++ gfg problems/hashing/1.cpp
@@ -0,0 +1,22 @@
+Zero Sum Subarrays 
+https://practice.geeksforgeeks.org/problems/zero-sum-subarrays1825/1
+class Solution{
+    public:
+    //Function to count subarrays with sum equal to 0.
+    ll findSubarray(vector<ll> arr, int n ) {
+        unordered_map<int, vector<int>> m;
+        int ans=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum = sum + arr[i];
+            if(sum==0){
+                ans++;
+            }
+            if(m.find(sum)!=m.end()){
+                ans+=m[sum].size();
+            }
+            m[sum].push_back(sum);
+        }
+        return ans;
+    }
+};
